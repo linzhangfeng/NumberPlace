9efd46c9df29da4822a29ce4b728aa8b8f3ccc63
Your Name <308127947@qq.com>
2016-08-24 17:41:47 +0800

    修改移动逻辑，添加背景框

--- src/scene/GameScene.js
+++ src/scene/GameScene.js
@@ -38,7 +38,7 @@ var GameScene = cc.Scene.extend({
 		cc.log("====>initLayer="+this._nLayerType);
 		switch(this._nLayerType){	
 		case GameConfig.GameList.GamePlay1:
-			pLayer = new GamePlaysLayer(this._nLayerType);
+			pLayer = new GameLayer(this._nLayerType);
 			break;
 		case GameConfig.GameList.GamePlay2:
 			pLayer = new GamePlaysLayer(this._nLayerType);
